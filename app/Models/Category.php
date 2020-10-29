f48a94c7b4229fa6253c6efd26103b77f87e47e7
Eduardo Fuentes <edu05feb2010@gmail.com>
2020-10-28 18:58:25 -0600

    wip

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Category
+ *
+ * @property-read int $id
+ * @property string $name
+ * @property string $thumbnail
+ * @property string $thumbnail_mime_type
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class Category extends Model
 {
     use HasFactory;
