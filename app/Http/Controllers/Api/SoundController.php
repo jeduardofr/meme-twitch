0649894fed7e7f0a8727be63c3c75c434d3c9090
Esau <andres.lopez4191@alumnos.udg.mx>
2020-11-01 19:15:07 -0600

    SoundController
    Se crearon las funciones controladores del sonido

--- app/Http/Controllers/Api/SoundController.php
+++ app/Http/Controllers/Api/SoundController.php
@@ -3,83 +3,68 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SoundResource;
+use App\Models\Sound;
+use App\Services\FileService;
 use Illuminate\Http\Request;
 
 class SoundController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    protected $fileService;
+
+
+    public function __construct(FileService $fileService)
+    {
+        $this->fileService = $fileService;
+    }
+
     public function index()
     {
         //Necesita paginacion  es un desmadre
+        return SoundResource::collection(Sound::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // public function create()
-    // {
-    //     //
-    // }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // Donde se llama a la base de datos para mostrar
     public function store(Request $request)
     {
-        //
+        $sound = Sound::create([
+            'keyword'               => $request->keyword,
+            'author'                => $request->author,
+            'audio'                 => $request->audio,
+            'audio_mime_type'       => $request->audio_mime_type,
+            'thumbnail'             => $request->thumnail,
+            'thumbnail_mime_type'   => $request->thumbnail_mime_type,
+        ]);
+
+        return new SoundResource($sound);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($sound)
+    public function update(Request $request, Sound $sound)
     {
-        //
+        if ($request->hasFile('thumbnail')) {
+            $this->fileService->removeIfExists('public/sounds/'. $sound->thumbnail);
+            $image = $this->fileService->upload('public/sounds/', $request->file('thumbnail'));
+            $sound->thumbnail = $image->name;
+            $sound->thumbnail_mime_type = $image->mime_type;
+        }
+
+        if ($request->has('url')) {
+            $this->fileService->removeIfExists('public/sounds/'. $sound->thumbnail);
+            $sound->thumbnail = $request->url;
+        }
+
+        $sound->name = $request->name ?? $sound->name;
+        $sound->audio = $request->audio ?? $sound->audio;
+        $sound->save();
+
+        return (new SoundResource($sound))->response()->setStatusCode(200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // public function edit($id)
-    // {
-    //     //
-    // }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy($sound)
     {
-        //
-    }
+        $this->fileService->removeIfExists('public/sounds/' . $sound->thumnail);
+        $sound->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()-> json();
     }
 }
--- app/Models/Sound.php
+++ app/Models/Sound.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Sound extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['keyword', 'author', 'audio', 'audio_mime_type', 'id_url', 'thumbnail', 'thumbnail_mime_type'];
+    protected $with = ['appaerances'];
+
+    public function getUrlAttribute()
+    {
+        if ($this->is_url) {
+            return $this->thumbnail;
+        }
+
+        return asset('storage/sounds/' . $this->thumbnail);
+    }
+
+    public function setThumbnailAttribute($value)
+    {
+        $this->attributes['thumbnail'] = $value;
+
+        $this->attributes['is_url'] = filter_var($value, FILTER_VALIDATE_URL) ? true : false;
+    }
 }
