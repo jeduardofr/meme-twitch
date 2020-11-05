755f73882ddf94ba04d5a24d932f46e11775bf22
Eduardo Fuentes <edu05feb2010@gmail.com>
2020-11-05 15:24:10 -0600

    Updated validation for category form
    The validation just to used not validate the file field. In this commit
    that is corrected so it properly works.

--- resources/ts/ui/category/validation.ts
+++ resources/ts/ui/category/validation.ts
@@ -1,21 +1,37 @@
 import * as yup from "yup";
 
-const schema = yup.object().shape(
-    {
-        name: yup.string().required("El nombre es obligatorio"),
-        url: yup.string().when(["file"], {
-            is: file => !file,
-            then: yup
-                .string()
-                .required("La URL es obligatoria")
-                .url("La URL no tiene un formato válido")
-        }),
-        file: yup.mixed().when("url", {
-            is: url => !url,
-            then: yup.mixed().required("La imagen es obligatoria")
-        })
-    },
-    [["file", "url"]]
-);
+const SUPPORTED_FORMATS = ["image/png", "image/jpeg"];
+const FILE_SIZE = 1024 * 1024 * 8;
+
+const schema = yup.object().shape({
+    name: yup.string().required("El nombre es obligatorio"),
+    type: yup.string(),
+    url: yup.string().when("type", {
+        is: "url",
+        then: yup
+            .string()
+            .required("La URL es obligatoria")
+            .url("La URL no tiene un formato válido")
+    }),
+    file: yup.mixed().when("type", {
+        is: "file",
+        then: yup
+            .mixed()
+            .required("El archivo es obligatorio")
+            .test(
+                "fileSize",
+                "El tamaño del archivo es muy grande",
+                value => value && value[0] && value[0].size <= FILE_SIZE
+            )
+            .test(
+                "fileType",
+                "El formato del archivo no es soportado",
+                value =>
+                    value &&
+                    value[0] &&
+                    SUPPORTED_FORMATS.includes(value[0].type)
+            )
+    })
+});
 
 export default schema;
