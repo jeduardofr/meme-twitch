91b780683fdd895845b3edb29a938c493d1e2672
Eduardo Fuentes <edu05feb2010@gmail.com>
2020-11-11 15:23:06 -0600

    Retrieve Profile(me) Service

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\SignInController;
 use App\Http\Controllers\Api\SignUpController;
 use App\Http\Controllers\Api\SoundController;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +26,10 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('auth/sign-up', [SignUpController::class, 'store']);
+Route::post('auth/sign-in', [SignInController::class, 'store']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/profile/me', [ProfileController::class, 'index']);
+});
+
 Route::resource('sounds', SoundController::class)->except(['edit', 'show', 'create']);
 Route::resource('categories', CategoryController::class)->except(['show', 'create', 'edit']);
